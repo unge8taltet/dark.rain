ed17aac7f1394f571ddb2e0ae400c985f9482834
unge8taltet <unge8taltet@gmail.com>
2022-07-01 16:23:24 +0300

    fix swiper

--- src/assets/js/routes/common.js
+++ src/assets/js/routes/common.js
@@ -1,8 +1,29 @@
-export default {
-    init() {
-        console.log('common init')
+import Swiper, {Navigation} from "swiper";
+
+new Swiper('.category__swiper', {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    navigation: {
+        nextEl: '.category__arrow--next',
+        prevEl: '.category__arrow--prev',
     },
-    finalize() {
-        console.log('common finalize')
-    }
-}
+    modules: [Navigation],
+});
+new Swiper('.bestsellers__swiper', {
+    spaceBetween: 30,
+    slidesPerView: 4,
+    navigation: {
+        nextEl: '.bestsellers__arrow--next',
+        prevEl: '.bestsellers__arrow--prev',
+    },
+    modules: [Navigation],
+});
+new Swiper('.inspiration__swiper', {
+    spaceBetween: 30,
+    slidesPerView: 4,
+    navigation: {
+        nextEl: '.inspiration__arrow--next',
+        prevEl: '.inspiration__arrow--prev',
+    },
+    modules: [Navigation],
+});
